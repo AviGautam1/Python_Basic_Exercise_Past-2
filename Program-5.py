236c8817b6fb76166591647bd7e171229dd6f881
AviGautam1 <avifetian@gmail.com>
2022-12-26 08:06:46 -0800

    Write a Python program to calculate the length of a string.

---
+++ Program-5.py
@@ -0,0 +1,14 @@
+# Write a Python program to calculate the length of a string.
+
+str = input("Enter Any String : ")
+
+# # counter variable to count the character in a string
+counter = 0
+for s in str:
+      counter = counter + 1
+print("Length of the input string is:", counter)
+
+'''
+Enter Any String : avinesh gautam
+Length of the input string is: 14
+'''
