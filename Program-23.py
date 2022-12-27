e88dd13b03bc60e8e85b3d4956bda73f47cca609
AviGautam1 <avifetian@gmail.com>
2022-12-27 00:40:07 -0800

    Write a Python program to check whether a string starts with specified characters.

---
+++ Program-23.py
@@ -0,0 +1,10 @@
+# Write a Python program to check whether a string starts with specified characters.
+
+str = "avinesh gautam"
+
+print(str.startswith("avi"))
+print(str.startswith("gau"))
+
+# output = True
+# False
+
