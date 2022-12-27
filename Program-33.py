543eec4c465c1987e946a0234e0434cb77d2fe9f
AviGautam1 <avifetian@gmail.com>
2022-12-27 01:48:41 -0800

    Write a Python program to count occurrences of a substring in a string.

---
+++ Program-33.py
@@ -0,0 +1,14 @@
+# Write a Python program to count occurrences of a substring in a string.
+
+str1 = "twinkle twinkle little star"
+str2 = "jony jony yes papa"
+
+print(str1.count("twinkle"))        # The count() method returns the number of elements with the specified value.
+print(str2.count("jony"))
+
+'''
+output = 
+2
+2
+'''
+
