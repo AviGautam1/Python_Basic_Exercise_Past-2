325437b61caf71f33a02a80ae0bf9655c5429dc9
AviGautam1 <avifetian@gmail.com>
2022-12-26 22:52:22 -0800

    Write a Python script that takes input from the user and displays that input back in upper and lower cases.

---
+++ Program-12.py
@@ -0,0 +1,17 @@
+# Write a Python script that takes input from the user and displays that input back in upper and lower cases.
+
+
+str = input("Enter Any Input : ")
+
+print("Your Upper Case Letter Is : ", str.upper())
+print("Your Lower Case Letter Is : ", str.lower())
+
+
+'''
+output = 
+Enter Any Input : Hi There Whats Going On
+Your Upper Case Letter Is :  HI THERE WHATS GOING ON
+Your Lower Case Letter Is :  hi there whats going on
+'''
+
+
