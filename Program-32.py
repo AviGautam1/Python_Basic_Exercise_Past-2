a3e611be4c9c8efe90d4ecd5e16d5229333da8f7
AviGautam1 <avifetian@gmail.com>
2022-12-27 01:40:56 -0800

    Write a Python program to format a number with a percentage.

---
+++ Program-32.py
@@ -0,0 +1,18 @@
+# Write a Python program to format a number with a percentage.
+
+
+x = 0.45
+y = -0.23
+print("Original Number: ", x)
+print("Formatted Number with percentage: "+"{:.2%}".format(x));
+print("Original Number: ", y)
+print("Formatted Number with percentage: "+"{:.2%}".format(y));
+
+
+'''
+output = 
+Original Number:  0.45
+Formatted Number with percentage: 45.00%
+Original Number:  -0.23
+Formatted Number with percentage: -23.00%
+'''
