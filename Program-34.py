19da5bf15a9db67c8a3efbc41fa5da9a8ff7c527
AviGautam1 <avifetian@gmail.com>
2022-12-27 01:53:36 -0800

    Write a Python program to reverse a string.

---
+++ Program-34.py
@@ -0,0 +1,15 @@
+# Write a Python program to reverse a string.
+
+#using slicing:
+string = "Hello world"
+print(string[::-1])
+
+# Reversing a String using reversed() function
+string = "Hello world"
+print(''.join(list(reversed(string))))
+
+'''
+output =
+dlrow olleH
+dlrow olleH
+'''
