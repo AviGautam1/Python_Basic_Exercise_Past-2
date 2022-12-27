5ba9854ba9cb09261b70a9217e4258a50703c619
AviGautam1 <avifetian@gmail.com>
2022-12-27 01:38:14 -0800

    Write a Python program to display a number with a comma separator.

---
+++ Program-31.py
@@ -0,0 +1,18 @@
+# Write a Python program to display a number with a comma separator.
+
+
+x = 6565443
+y = 665373722
+print("Original Number: ", x)
+print("Formatted Number with comma separator: "+"{:,}".format(x));
+print("Original Number: ", y)
+print("Formatted Number with comma separator: "+"{:,}".format(y));
+
+
+'''
+output = 
+Original Number:  6565443
+Formatted Number with comma separator: 6,565,443
+Original Number:  665373722
+Formatted Number with comma separator: 665,373,722
+'''
